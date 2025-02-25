cc5f0c47505e3186a2ae760f6c1a863ea357b985
kishanlalchoudhary <kisanchoudhary000@gmail.com>
2025-02-25 22:57:56 +0530

    Added Skills Questions Route

--- controllers/skill.controller.js
+++ controllers/skill.controller.js
@@ -1,5 +1,6 @@
 const prisma = require("../config/prisma");
 const { deleteFileFromCloudinary } = require("../utils/cloudinary.utility");
+const model = require("../config/gemini");
 
 const addSkill = async (req, res) => {
   try {
@@ -68,7 +69,51 @@ const removeSkill = async (req, res) => {
   }
 };
 
-const getQuestions = async (req, res) => {};
+const getQuestions = async (req, res) => {
+  try {
+    const skill = "DBMS";
+    const prompt = `Generate an array of 3 easy and 3 easy-to-medium level interview questions on ${skill}, suitable for a fresher-level interview. Ensure the questions are relevant to real-world applications and commonly asked in technical interviews. Each question should be concise, unambiguous, and answerable within 250 words. Strictly output only the questions in the following format:
+
+    { "questions" : [
+      {
+        "question": "Question 1"
+      },
+      {
+        "question": "Question 2"
+      },
+      {
+        "question": "Question 3"
+      },
+      {
+        "question": "Question 4"
+      },
+      {
+        "question": "Question 5"
+      },
+      {
+        "question": "Question 6"
+      }
+  ]}`;
+
+    const response = await model.invoke(prompt);
+    const textResponse = response.content;
+
+    // Extract valid JSON
+    let questions = null;
+    const jsonMatch = textResponse.match(/\[.*\]/s);
+    if (jsonMatch) {
+      const jsonData = JSON.parse(jsonMatch[0]);
+      questions = jsonData;
+      console.log(jsonData);
+    } else {
+      console.error("Invalid JSON format received");
+    }
+    res.status(200).json({ questions });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+};
 
 const evaluateAnswers = async (req, res) => {};
 
